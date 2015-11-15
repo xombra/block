6fdf467e6276037fa3ac17ce2be6209f6b38fabc
Xombra <xombra.com@gmail.com>
2015-11-15 16:39:32 -0430

    * bugfixed

--- block.sh
+++ block.sh
@@ -5,7 +5,7 @@
 # bloquear url
 
 if [[ $USER != root ]]; then
-	echo -e "\e[00;31mERROR: Debe ser ROOT para ejecutar el Script\e[00m"
+	echo -e "\e[00;31mERROR: You must be root to run script\e[00m"
 	exit 1
 fi
 
@@ -16,14 +16,12 @@ else
 	touch /etc/hosts
 fi
 
-
 # formato para este archivo:
 # IP  domain
 # Es valido el uso de comodines en dominio
 # It is allowed to use wildcards (*) when specifying the domain.
 # Write file /etc/hosts
 
-	echo "127.0.0.1" $1 >> /etc/hosts
-	echo $1 " It was written in the host file" 
-	exit 0
-
+echo "127.0.0.1" $1 >> /etc/hosts
+echo $1 " It was written in the host file" 
+exit 0
